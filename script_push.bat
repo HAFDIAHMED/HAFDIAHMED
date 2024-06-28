f7d8f941d0f5d4c05c2ba8abebca08907e61ffc9
AHMED HAFDI <ahafdi62@gmail.com>
2024-06-28 09:26:38 +0100

    Automatic commit at 28/06/2024  9:26:38,30

---
+++ script_push.bat
@@ -0,0 +1,20 @@
+@echo off
+cd C:\Users\018801\Documents\Github\HAFDIAHMED
+setlocal ENABLEDELAYEDEXPANSION
+set /a counter=0
+
+:loop
+echo Hello Commit > commit_message.txt
+git add .
+git commit -m "Automatic commit at %date% %time%"
+git push -f
+set /a counter=!counter!+1
+
+rem Check if the counter is 5 (approximately 10 minutes)
+if !counter! EQU 5 (
+    del *.txt
+    set /a counter=0
+)
+
+timeout /t 30
+goto loop
