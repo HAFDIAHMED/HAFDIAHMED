24bde793ce26812291b40256d0f638f868ac4bc0
AHMED HAFDI <ahafdi62@gmail.com>
2024-06-28 09:28:48 +0100

    Automatic commit at 28/06/2024  9:28:48,10

--- script_push.bat
+++ script_push.bat
@@ -1,20 +1,9 @@
 @echo off
 cd C:\Users\018801\Documents\Github\HAFDIAHMED
-setlocal ENABLEDELAYEDEXPANSION
-set /a counter=0
-
 :loop
 echo Hello Commit > commit_message.txt
 git add .
 git commit -m "Automatic commit at %date% %time%"
 git push -f
-set /a counter=!counter!+1
-
-rem Check if the counter is 5 (approximately 10 minutes)
-if !counter! EQU 1 (
-    del *.txt
-    set /a counter=0
-)
-
 timeout /t 3
 goto loop
