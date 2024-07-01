ab5bae3850832496ed2aaf0bf61c6ff9c0e85cff
AHMED HAFDI <ahafdi62@gmail.com>
2024-07-01 08:23:58 +0100

    Automatic commit at 01/07/2024  8:23:58,57

--- script_push.bat
+++ script_push.bat
@@ -2,7 +2,7 @@
 :loop
 cd C:\Users\018801\Documents\Github\HAFDIAHMED
 
-echo Hello Commit > commit_message.txt
+echo "Automatic commit at %date% %time%" > commit_message.txt
 git add .
 git commit -m "Automatic commit at %date% %time%"
 git push -f
