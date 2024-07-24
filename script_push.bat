fdbd152fdca1422426e35b1cf769bba450fcf660
AHMED HAFDI <ahafdi62@gmail.com>
2024-07-24 08:08:47 +0100

    Automatic commit at 24/07/2024  8:08:47,23

--- script_push.bat
+++ script_push.bat
@@ -6,6 +6,6 @@ echo "Automatic commit at %date% %time%" > commit_message.txt
 git add .
 git commit -m "Automatic commit at %date% %time%"
 git push -f
-timeout /t 500 /nobreak
+timeout /t 100 /nobreak
 
 goto loop
