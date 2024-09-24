9b702f6b5fb4cc3ec4bc8eedb7e818e4e84b9f5f
HAFDIAHMED <>
2024-09-24 09:20:04 +0100

    Automatic commit at 24/09/2024  9:20:04,76

--- script_push.bat
+++ script_push.bat
@@ -3,6 +3,7 @@
 cd C:\Users\018801\Documents\Github\HAFDIAHMED
 
 echo "Automatic commit at %date% %time%" > commit_message.txt
+git pull
 git add .
 git commit -m "Automatic commit at %date% %time%"
 git push -f
